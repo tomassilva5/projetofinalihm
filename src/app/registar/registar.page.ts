63ca264cd1ec943a3e4e946a7402d828a83a4513
tomas <tomas.silva@ipvc.pt>
2025-05-14 22:12:55 +0100

    Começo da página Registar

---
+++ src/app/registar/registar.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonButton, IonItem } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-registar',
+  templateUrl: './registar.page.html',
+  styleUrls: ['./registar.page.scss'],
+  standalone: true,
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    IonInput, IonButton, IonItem,
+    CommonModule, FormsModule
+  ]
+})
+export class RegistarPage {
+  nome = '';
+  email = '';
+  telefone = '';
+  password = '';
+
+  onSubmit() {
+    alert(`Nome: ${this.nome}\nEmail: ${this.email}\nTelefone: ${this.telefone}`);
+  }
+
+  voltar() {
+    window.history.back();
+  }
+}
